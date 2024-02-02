3c553473f3177281508e00d79d12270ae95001a5
Ansh Choudhary <32743873+AnshChoudhary@users.noreply.github.com>
2024-02-02 10:55:21 +0530

    initial upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { ColorModeContext, useMode } from './theme';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [theme, colorMode] = useMode();
+
+  return (<ColorModeContext.Provider value= {colorMode}>
+    <div className="app"></div>;
+    </ColorModeContext.Provider>
   );
 }
 
