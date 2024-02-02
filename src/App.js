6611410eb6a8e264aa79f9e8c06bb40b323fb98c
Ansh Choudhary <32743873+AnshChoudhary@users.noreply.github.com>
2024-02-02 14:44:14 +0530

    topbar implement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import Topbar from './scenes/global/Topbar';
 import { ColorModeContext, useMode } from './theme';
 
 
 function App() {
   const [theme, colorMode] = useMode();
 
-  return (<ColorModeContext.Provider value= {colorMode}>
-    <div className="app"></div>;
-    </ColorModeContext.Provider>
+  return (
+  <ColorModeContext.Provider value= {colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="app">
+        <main className="content">
+          <Topbar />
+        </main>
+      </div>
+    </ThemeProvider>
+  </ColorModeContext.Provider>
   );
 }
 
--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,5 +1,51 @@
+import { Box,IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../themes";
+import InputBase from "@mui/material/InputBase";
+import { LightModeOutlinedIcon } from "@mui/icons-material/LightModeOutlined";
+import { DarkModeOutlinedIcon } from "@mui/icons-material/LightModeOutlined";
+import { NotificationsOutlinedIcon } from "@mui/icons-material/LightModeOutlined";
+import { SettingsOutlinedIcon } from "@mui/icons-material/LightModeOutlined";
+import { PersonOutlinedIcon } from "@mui/icons-material/LightModeOutlined";
+import { SearchOutlinedIcon } from "@mui/icons-material/LightModeOutlined";
+
 const Topbar = () => {
-    return <div> Topbar</div>
-}
+    const theme = useTheme();
+    const colors = tokens(theme.paletter.mode);
+    const colorMode = useContext(ColorModeContext);
+
+    return <Box display="flex" justifyContent="space-between" p={2}>
+        {}
+        <Box 
+            display='flex' 
+            backgroundColor={colors.primary[400]}
+            borderRadius= "3px"
+        >
+            <InputBase sx={{ml:2, fl:1}} placeholder="Search" />
+            <IconButton>
+                <SearchIcon />
+            </IconButton>
+        </Box>
+
+        {/* ICONS */}
+        <Box display='flex'>
+            <IconButton onClick={colorMode.toggleColorMode}>
+                {theme.palette.mode === 'dark' ? (
+                    <DarkModeOutlinedIcon />
+                    ) : (<LightModeOutlinedIcon />)
+                }
+            </IconButton>
+            <IconButton>
+                <NotificationsOutlinedIcon />
+            </IconButton>
+            <IconButton>
+                <SettingsOutlinedIcon />
+            </IconButton>
+            <IconButton>
+                <PersonOutlinedIcon />
+            </IconButton>
+        </Box>
+    </Box>;
+};
 
 export default Topbar; 
